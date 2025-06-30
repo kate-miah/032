80a3d09c6600ef47b087fc21f66c9846dcf6b9ba
kate-miah <ekaterin89@gmail.com>
2025-06-30 13:36:24 -0500

    032 -tasks

---
+++ index.js
@@ -0,0 +1,39 @@
+// 1
+let a = 5;
+let b = 10;
+console.log(a * b);
+
+// 2
+let c = 10;
+let d = 2;
+console.log(c / d);
+
+3
+let num = 1;
+num++; // 2
+console.log(num);
+num--; // 1
+console.log(num);
+num+= 22; // 23
+console.log(num);
+num*= 33; // 759
+console.log(num);
+num/= 44; // 17.25
+console.log(num);
+num-= 55; // 37.75
+console.log(num);
+
+//4
+let num1 = 1;
+++num1; // 2
+console.log(num1);
+--num1; // 1
+console.log(num1);
+num1+= 22; // 23
+console.log(num1);
+num1*= 33; // 759
+console.log(num1);
+num1/= 44; // 17.25
+console.log(num1);
+num1-= 55; // 37.75
+console.log(num1);
